b816a3e2021ebdad08b2ccd272bacbd3ef5eddcf
sk1zzovsmx <71350307+sk1zzovsmx@users.noreply.github.com>
2025-06-14 19:08:14 +0200

    folder libs

--- !KRT/Libs/LibBossIDs-1.0/LibBossIDs-1.0.lua
+++ !KRT/Libs/LibBossIDs-1.0/LibBossIDs-1.0.lua
@@ -1,6 +1,6 @@
---[[
+﻿--[[
 Name: LibBossIDs-1.0
-Revision: $Revision: 48 $
+Revision: $Revision: 44 $
 Author: Elsia
 Website: http://www.wowace.com/addons/libbossids-1-0/
 Documentation:
@@ -10,7 +10,7 @@ Dependencies: LibStub
 License: Public Domain, Absolutely no Warranty.
 ]]
 
-local MAJOR, MINOR = "LibBossIDs-1.0", "$Revision: 48 $"
+local MAJOR, MINOR = "LibBossIDs-1.0", "$Revision: 44 $"
 local lib = LibStub:NewLibrary(MAJOR, MINOR)
 
 if not lib then return end
@@ -958,75 +958,6 @@ local BossIDs = {
 	[39899] = true, -- Baltharus (Copy has an own id apparently)
 	[40142] = true, -- Halion (twilight realm)
 
-    -- Blackrock Mountain: Blackrock Caverns
-    [39665] = true, 	-- Rom'ogg Bonecrusher
-    [39679] = true, 	-- Corla, Herald of Twilight
-    [39698] = true, 	-- Karsh Steelbender
-    [39700] = true, 	-- Beauty
-    [39705] = true, 	-- Ascendant Lord Obsidius
-
-    -- Abyssal Maw: Throne of the Tides
-    [40586] = true, 	-- Lady Naz'jar
-    [40765] = true, 	-- Commander Ulthok
-    [40825] = true, 	-- Erunak Stonespeaker
-    [40788] = true, 	-- Mindbender Ghur'sha
-    [42172] = true, 	-- Ozumat
-
-    -- The Stonecore
-    [43438] = true, 	-- Corborus
-    [43214] = true, 	-- Slabhide
-    [42188] = true, 	-- Ozruk
-    [42333] = true, 	-- High Priestess Azil
-
-    -- The Vortex Pinnacle
-    [43878] = true, 	-- Grand Vizier Ertan
-    [43873] = true, 	-- Altairus
-    [43875] = true, 	-- Asaad
-
-    -- Grim Batol
-    [39625] = true, 	-- General Umbriss
-    [40177] = true, 	-- Forgemaster Throngus
-    [40319] = true, 	-- Drahga Shadowburner
-    [40484] = true, 	-- Erudax
-
-    -- Halls of Origination
-    [39425] = true, 	-- Temple Guardian Anhuur
-    [39428] = true, 	-- Earthrager Ptah
-    [39788] = true, 	-- Anraphet
-    [39587] = true, 	-- Isiset
-    [39731] = true, 	-- Ammunae
-    [39732] = true, 	-- Setesh
-    [39378] = true, 	-- Rajh
-
-    -- Lost City of the Tol'vir
-    [44577] = true, 	-- General Husam
-    [43612] = true, 	-- High Prophet Barim
-    [43614] = true, 	-- Lockmaw
-    [49045] = true, 	-- Augh
-    [44819] = true, 	-- Siamat
-
-    -- Baradin Hold
-    [47120] = true, 	-- Argaloth
-	
-    -- Blackrock Mountain: Blackwing Descent
-    [41570] = true, 	-- Magmaw
-    [42180] = true, 	-- Toxitron
-    [41378] = true, 	-- Maloriak
-    [41442] = true, 	-- Atramedes
-    [43296] = true, 	-- Chimaeron
-    [41376] = true, 	-- Nefarian
-	
-    -- Throne of the Four Winds
-    [45871] = true,	-- Nezir
-    [46753] = true,	-- Al'Akir
-	
-    -- The Bastion of Twilight
-    [45992] = true, 	-- Valiona
-    [45993] = true, 	-- Theralion
-    [44600] = true, 	-- Halfus Wyrmbreaker
-    [43735] = true, 	-- Elementium Monstrosity
-    [43324] = true, 	-- Cho'gall
-	
 	-- World Dragons
 	[14889] = true, -- Emeriss
 	[14888] = true, -- Lethon
