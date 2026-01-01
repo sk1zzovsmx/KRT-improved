e826ecec77b918dae5f575eaaea661963ec420a1
sk1zzovsmx <71350307+sk1zzovsmx@users.noreply.github.com>
2026-01-01 16:35:15 +0000

    fix C.UPDATE_INTERVAL_SPAMMER

--- !KRT/Modules/C.lua
+++ !KRT/Modules/C.lua
@@ -1,15 +1,15 @@
-local addonName, addon = ...
-addon.C               = addon.C or {}
+local addonName, addon        = ...
+addon.C                       = addon.C or {}
 
-local C               = addon.C
-local L               = addon.L
+local C                       = addon.C
+local L                       = addon.L
 
-C.ITEM_LINK_PATTERN =
+C.ITEM_LINK_PATTERN           =
     "|?c?(%x*)|?H?([^:]*):?(%d+):?(%d*):?(%d*):?(%d*):?(%d*):?(%d*):?" ..
     "(%-?%d*):?(%-?%d*):?(%d*)|?h?%[?([^%[%]]*)%]?|?h?|?r?"
 
 -- Roll Types Enum
-C.rollTypes = {
+C.rollTypes                   = {
     MAINSPEC   = 1,
     OFFSPEC    = 2,
     RESERVED   = 3,
@@ -20,7 +20,7 @@ C.rollTypes = {
 }
 
 -- Roll Type Display Text
-C.lootTypesText = {
+C.lootTypesText               = {
     L.BtnMS,
     L.BtnOS,
     L.BtnSR,
@@ -31,7 +31,7 @@ C.lootTypesText = {
 }
 
 -- Roll Type Colored Display Text
-C.lootTypesColored = {
+C.lootTypesColored            = {
     "|cff20ff20" .. L.BtnMS .. "|r",
     "|cffffff9f" .. L.BtnOS .. "|r",
     "|cffa335ee" .. L.BtnSR .. "|r",
@@ -42,7 +42,7 @@ C.lootTypesColored = {
 }
 
 -- Item Quality Colors
-C.itemColors = {
+C.itemColors                  = {
     [1] = "ff9d9d9d", -- Poor
     [2] = "ffffffff", -- Common
     [3] = "ff1eff00", -- Uncommon
@@ -53,7 +53,7 @@ C.itemColors = {
 }
 
 -- Class Colors
-C.CLASS_COLORS = {
+C.CLASS_COLORS                = {
     ["UNKNOWN"]     = "ffffffff",
     ["DEATHKNIGHT"] = "ffc41f3b",
     ["DRUID"]       = "ffff7d0a",
@@ -68,7 +68,7 @@ C.CLASS_COLORS = {
 }
 
 -- Raid Target Markers
-C.RAID_TARGET_MARKERS = {
+C.RAID_TARGET_MARKERS         = {
     "{circle}",
     "{diamond}",
     "{triangle}",
@@ -78,25 +78,25 @@ C.RAID_TARGET_MARKERS = {
     "{skull}",
 }
 
-C.K_COLOR  = "fff58cba"
-C.RT_COLOR = "aaf49141"
-C.titleString = "|c" .. C.K_COLOR .. "K|r|c" .. C.RT_COLOR .. "RT|r : %s"
+C.K_COLOR                     = "fff58cba"
+C.RT_COLOR                    = "aaf49141"
+C.titleString                 = "|c" .. C.K_COLOR .. "K|r|c" .. C.RT_COLOR .. "RT|r : %s"
 
-C.CHAT_OUTPUT_FORMAT = "%s: %s"
-C.CHAT_PREFIX = "Kader Raid Tools"
-C.CHAT_PREFIX_SHORT = "KRT"
-C.CHAT_PREFIX_HEX = C.K_COLOR
+C.CHAT_OUTPUT_FORMAT          = "%s: %s"
+C.CHAT_PREFIX                 = "Kader Raid Tools"
+C.CHAT_PREFIX_SHORT           = "KRT"
+C.CHAT_PREFIX_HEX             = C.K_COLOR
 
-C.UPDATE_INTERVAL_MASTER = 0.05
-C.UPDATE_INTERVAL_RESERVES = 0.5
-C.UPDATE_INTERVAL_CONFIG = 0.1
-C.UPDATE_INTERVAL_WARNINGS = 0.1
-C.UPDATE_INTERVAL_CHANGES = 0.1
-C.UPDATE_INTERVAL_SPAMMER = 0.05
-C.UPDATE_INTERVAL_HISTORY = 0.1
-C.LOOT_COUNTER_TICK_INTERVAL = 0.1
+C.UPDATE_INTERVAL_MASTER      = 0.05
+C.UPDATE_INTERVAL_RESERVES    = 0.5
+C.UPDATE_INTERVAL_CONFIG      = 0.1
+C.UPDATE_INTERVAL_WARNINGS    = 0.1
+C.UPDATE_INTERVAL_CHANGES     = 0.1
+C.UPDATE_INTERVAL_SPAMMER     = 0.1
+C.UPDATE_INTERVAL_HISTORY     = 0.1
+C.LOOT_COUNTER_TICK_INTERVAL  = 0.1
 
-C.LOOT_COUNTER_ROW_HEIGHT = 25
-C.RESERVES_ROW_HEIGHT = 42
-C.RESERVE_HEADER_HEIGHT = 24
+C.LOOT_COUNTER_ROW_HEIGHT     = 25
+C.RESERVES_ROW_HEIGHT         = 42
+C.RESERVE_HEADER_HEIGHT       = 24
 C.RESERVES_ITEM_FALLBACK_ICON = "Interface\\Icons\\INV_Misc_QuestionMark"
